acd87291c989fa4c9de4ef76b6b5be2a46e42193
Paveshen Padayachee <istimadan@gmail.com>
2026-09-07 01:59:24 +0200

    add account protobuf defs

---
+++ src/Demo.GRPC/Demo.GRPC.Endpoint/ProtoHandlers/AccountHandler.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace Demo.GRPC.Endpoint.ProtoHandlers;
+
+public class AccountHandler : Account.AccountBase
+{
+    public override Task<BalanceResponse> Balance(BalanceRequest request, ServerCallContext context)
+    {
+        return base.Balance(request, context);
+    }
+
+    public override Task<StatementExport> Statement(StatementExportRequest request, ServerCallContext context)
+    {
+        return base.Statement(request, context);
+    }
+}
---
+++ src/Demo.GRPC/Demo.StorageEngine/Services/StorageService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.StorageEngine.Services;
+
+public class StorageService
+{
+}
